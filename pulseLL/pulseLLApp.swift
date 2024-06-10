0baf11448969c77e21dedb59caeb87829f591660
tristanhaeuser <tristanhaeuser@icloud.com>
2024-06-10 11:08:28 +0200

    Initial Commit

---
+++ pulseLL/pulseLLApp.swift
@@ -0,0 +1,17 @@
+//
+//  pulseLLApp.swift
+//  pulseLL
+//
+//  Created by Tristan Häuser on 10.06.24.
+//
+
+import SwiftUI
+
+@main
+struct pulseLLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
