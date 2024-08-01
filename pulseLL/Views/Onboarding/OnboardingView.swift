2780b7602b9b087630f8c885c7b9580a0c783183
tristanhaeuser <tristan@haeuser.org>
2024-08-01 16:00:42 +0200

    Resolved Merge Conflict
    Signed-off-by: tristanhaeuser <tristan@haeuser.org>

--- pulseLL/Views/Onboarding/OnboardingView.swift
+++ pulseLL/Views/Onboarding/OnboardingView.swift
@@ -15,44 +15,26 @@ struct OnboardingView: View {
     
     var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-                OnboardingDropdown(label: "Workout", selectedOption: $selectedWorkout, options: ["Running", "Cycling", "Hiking"])
-                    .onChange(of: selectedWorkout) {
-                         switch selectedWorkout {
-                        case "Running":
-                            workoutManager.workoutType = .running
-                        case "Cycling":
-                            workoutManager.workoutType = .cycling
-                        case "Hiking":
-                            workoutManager.workoutType = .hiking
-                        default:
-                            workoutManager.workoutType = .running
-                        }
-                    }
-                        
-                OnboardingDropdown(label: "Genre", selectedOption: $selectedGenre, options: ["Techno", "Rock", "Dubstep"])
-                    .onChange(of: selectedGenre) { oldGenre, newGenre in
-                                        workoutManager.songGenre = newGenre
-                                    }
-                
-                Spacer()
-                
-                NavigationLink{WorkoutView()}label: {
-                    Image(systemName: "play.circle.fill")
-                        .resizable()
-                        .frame(width: 70, height: 70)
-                        .padding(.trailing)
-                        .foregroundColor(.black)
             ZStack {
                 VStack {
-                    OnboardingDropdown(label: "Workout", selectedOption: $selectedWorkout, options: ["Running", "Cycling", "Swimming"])
-                        .onChange(of: selectedGenre) { oldWorkout, newWorkout in
-                                            workoutManager.songGenre = newWorkout
-                                        }
-                    OnboardingDropdown(label: "Genre", selectedOption: $selectedGenre, options: ["Techno", "Rock", "EDM", "House"])
+                    OnboardingDropdown(label: "Workout", selectedOption: $selectedWorkout, options: ["Running", "Cycling", "Hiking"])
+                        .onChange(of: selectedWorkout) {
+                            switch selectedWorkout {
+                            case "Running":
+                                workoutManager.workoutType = .running
+                            case "Cycling":
+                                workoutManager.workoutType = .cycling
+                            case "Hiking":
+                                workoutManager.workoutType = .hiking
+                            default:
+                                workoutManager.workoutType = .running
+                            }
+                        }
+                    
+                    OnboardingDropdown(label: "Genre", selectedOption: $selectedGenre, options: ["Techno", "Rock", "Dubstep"])
                         .onChange(of: selectedGenre) { oldGenre, newGenre in
-                                            workoutManager.songGenre = newGenre
-                                        }
+                            workoutManager.songGenre = newGenre
+                        }
                     
                     Spacer()
                     
